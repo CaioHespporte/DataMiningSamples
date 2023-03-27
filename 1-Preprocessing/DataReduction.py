4440a62ab05da8d10b9f6c2d22d24cf27ba01efc
CaioHespporte <73289696+CaioHespporte@users.noreply.github.com>
2023-03-27 01:20:44 -0300

    S

--- 1-Preprocessing/DataReduction.py
+++ 1-Preprocessing/DataReduction.py
@@ -56,7 +56,7 @@ def VisualizePcaProjection(finalDf, targetColumn):
     ax.set_xlabel('Principal Component 1', fontsize = 15)
     ax.set_ylabel('Principal Component 2', fontsize = 15)
     ax.set_title('2 component PCA', fontsize = 20)
-    targets = [0, 1, ]
+    targets = [0,1]
     colors = ['r', 'g']
     for target, color in zip(targets,colors):
         indicesToKeep = finalDf[targetColumn] == target
